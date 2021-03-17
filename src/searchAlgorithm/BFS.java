b4561fd05851d1cf951617ed63a99eed2a2943ca
jonghaha <bgh9197@naver.com>
2021-03-17 22:02:07 +0900

    DFS, BFS 탐색 알고리즘 연습

---
+++ src/searchAlgorithm/BFS.java
@@ -0,0 +1,67 @@
+package searchAlgorithm;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+    public static boolean[] visited = new boolean[9];
+    public static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+    // BFS 함수.
+    public static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        // 큐에 삽입.
+        q.offer(start);
+        // 현재 노드 방문 처리.
+        visited[start] = true;
+        // 빈 큐일때까지.
+        while(!q.isEmpty()) {
+            // 큐에서 하나를 출력.
+            int x = q.poll();
+            System.out.print(x + " ");
+            // 방문하지 않은 노드를 큐에 삽입.
+            for(int i = 0; i < graph.get(x).size(); i++) {
+                int y = graph.get(x).get(i);
+                if(!visited[y]) {
+                    q.offer(y);
+                    visited[y] = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 9; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        graph.get(1).add(2);
+        graph.get(1).add(3);
+        graph.get(1).add(8);
+
+        graph.get(2).add(1);
+        graph.get(2).add(7);
+
+        graph.get(3).add(1);
+        graph.get(3).add(4);
+        graph.get(3).add(5);
+
+        graph.get(4).add(3);
+        graph.get(4).add(5);
+
+        graph.get(5).add(3);
+        graph.get(5).add(4);
+
+        graph.get(6).add(7);
+
+        graph.get(7).add(2);
+        graph.get(7).add(6);
+        graph.get(7).add(8);
+
+        graph.get(8).add(1);
+        graph.get(8).add(7);
+
+        bfs(1);
+    }
+}
