651939e35e559e1a5be57430f26f090457e5ee10
jonghaha <bgh9197@naver.com>
2021-04-11 23:31:28 +0900

    프로그래머스 문제 풀기(더 맵게)

---
+++ src/programmers/Spicier.java
@@ -0,0 +1,35 @@
+package programmers;
+
+import java.util.PriorityQueue;
+
+public class Spicier {
+    static int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> heap = new PriorityQueue();
+
+        for (int aScoville : scoville) {
+            heap.offer(aScoville);
+        }
+
+        while (heap.peek() <= K) {
+            if (heap.size() == 1) {
+                return -1;
+            }
+            int a = heap.poll();
+            int b = heap.poll();
+
+            int result = a + (b * 2);
+
+            heap.offer(result);
+            answer ++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        // return 2
+        int[] scoville = {1, 2, 3, 9, 10, 12};
+        int K = 7;
+        System.out.println(solution(scoville, K));
+    }
+}
