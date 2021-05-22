217ada4f3c07d175df650a220fac14cf74641d5c
bgh9197@naver.com <bgh9197@naver.com>
2021-05-22 18:53:57 +0900

    지그제그 배열 연습

--- src/programmers/ZigZeg.java
+++ src/programmers/ZigZeg.java
@@ -3,11 +3,11 @@ package programmers;
 public class ZigZeg {
     // n 행렬의 크기, r 행, c 열
     static long solution(int n, int r, int c) {
-//       long[][] nMatrix = new long[n][n];
-       int x = 0, y = 0;
-       long count = 1;
+        long[][] nMatrix = new long[n][n];
+        int x = 0, y = 0;
+        long count = 1;
 
-//       nMatrix[x][y] = count++;
+        nMatrix[x][y] = count++;
 
         while(count < n * n) {
             if(y + 1 < n) {
@@ -16,13 +16,9 @@ public class ZigZeg {
                 x++;
             }
 
-            if((r-1) == x && (c-1) == y) return count++;
-            count++;
-//            nMatrix[x][y] = count++;
+            nMatrix[x][y] = count++;
             while(y - 1 > -1 && x + 1 < n) {
-                if((r-1) == x && (c-1) == y) return count++;
-//                nMatrix[++x][--y] = count++;
-                x++; y--; count++;
+                nMatrix[++x][--y] = count++;
             }
 
             if(x + 1 < n) {
@@ -30,21 +26,24 @@ public class ZigZeg {
             } else {
                 y++;
             }
-            if((r-1) == x && (c-1) == y) return count++;
-            count++;
-//            nMatrix[x][y] = count++;
+            nMatrix[x][y] = count++;
             while(x - 1 > -1 && y + 1 < n) {
-                if((r-1) == x && (c-1) == y) return count++;
-//                nMatrix[--x][++y] = count++;
-                x--; y++; count++;
+                nMatrix[--x][++y] = count++;
             }
+        }
+
+        for(long[] a : nMatrix) {
+            for(long b : a) {
+                System.out.print(b + " ");
+            }
+            System.out.println();
         }
 
         return count;
     }
 
     public static void main(String[] agrs) {
-        int n = 5;
+        int n = 6;
         int r = 3;
         int c = 4;
 
